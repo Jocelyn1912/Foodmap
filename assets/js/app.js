0895d1511a0353e938a7d9662c5f666fdab41149
Jocelyn1912 <toty_2410@hotmail.com>
2017-12-13 15:30:22 -0300

    modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,42 @@
-
 /*
-*Función jQuery que permite que se ejecute Splash Screen
-
-$(document).ready(function () {
-  $('.splash-screen').fadeOut(4000);
-})
+*Función principal
 */
-
 $(document).ready(function(){
-  $(".img").mouseover(function(){
-    $(".img").css("color", "yellow");
+  /*
+  *Función jQuery que permite que se ejecute Splash Screen
+  */
+  $('.splash-screen').fadeOut(4000);
+
+  /*
+  *Función filtro, ocupé el evento change ya que corresponde a la etiqueta select.
+  */
+  $('select').change(function() {
+    var textoFiltro = $(this).val()
+    if(textoFiltro == 'todas'){
+      $('div.filter div.hidden').fadeIn('slow').removeClass('hidden');
+    } else {
+      $('.filter div').each(function() {
+        if(!$(this).hasClass(textoFiltro)) {
+          $(this).fadeOut('normal').addClass('hidden');
+        } else {
+          $(this).fadeIn('slow').removeClass('hidden');
+        }
+      });
+    }
+    return false;
   });
-  $(".img").mouseout(function(){
-    $(".img").css("color", "lightgray");
+
+  /*
+  *Función que muestra el nombre del restorante al posisionarse sobre la foto
+  */
+  $('.contenedor1').mouseover(function () {
+  $('.food-overlay1').show();
+  }).mouseout(function () {
+    $('.food-overlay1').hide();
   });
+
+
+
+
+
 });
